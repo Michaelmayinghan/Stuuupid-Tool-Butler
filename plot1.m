9a1b103178e7e74a0310795a7a1737971ef8ea3b
milaLuo <luoshanyuan1206@gmail.com>
2026-02-27 21:36:30 +0000

    code written in Fridays programming mentor session - Naerthi

---
+++ plot1.m
@@ -0,0 +1,17 @@
+close
+clc % cannot clear
+
+plot(Position.longitude,Position.latitude);
+figure(2);
+plot(Position.Timestamp,Position.latitude, 'r');
+
+x1 = round(Position.latitude(1),2)
+x2 = round(Position.latitude(50),2)
+
+if x1 == x2
+    disp("Stop")
+end
+
+
+
+
