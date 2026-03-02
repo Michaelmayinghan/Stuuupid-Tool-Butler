e8b19f838468316e44e7fd472ea158acebd2b353
milaLuo <luoshanyuan1206@gmail.com>
2026-03-02 13:01:01 +0000

    2Mar-Test for Angular Velocity

---
+++ AngularV.m
@@ -0,0 +1,18 @@
+close
+clc % cannot clear
+
+plot(Position.longitude,Position.latitude);
+figure(2);
+plot(Position.Timestamp,Position.latitude, 'r');
+plot(AngularVelocity.Timestamp,AngularVelocity.x, 'r');
+
+x1 = round(Position.latitude(1),2)
+x2 = round(Position.latitude(50),2)
+
+if x1 == x2
+    disp("Stop")
+end
+
+
+
+
