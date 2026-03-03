d8786b843db29e07e89a525c695f6f16fb024a59
milaLuo <luoshanyuan1206@gmail.com>
2026-03-03 11:54:46 +0000

    update

--- AngularV.m
+++ AngularV.m
@@ -11,6 +11,18 @@ plot(AngularVelocity.Timestamp,AngularVelocity.Z, 'r');
 % This Z is fine
 
 
+figure(2)
+plot(AngularVelocity.Timestamp, AngularVelocity.X, 'b')
+hold on
+plot(AngularVelocity.Timestamp, AngularVelocity.Y, 'g')
+plot(AngularVelocity.Timestamp, AngularVelocity.Z, 'r')
+hold off
+legend('X','Y','Z')
+grid on
+
+
+class(AngularVelocity.Timestamp)
+
 
 
 
